538502dcd8010c7802eb143d8fbd0857e6d3f6dc
0seo8 <00seo23@gmail.com>
2023-07-23 20:57:51 +0900

    design: header 디자인 수정

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,45 @@
 import * as S from './styles';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <S.Header>
-      <S.MenuBar>
-        <a href="/">PROJECTS</a>&nbsp;/&nbsp;<a href="/">INFORMATION</a>
-      </S.MenuBar>
+      <S.HeaderContainer>
+        <S.LogoLink>DBYFO</S.LogoLink>
+        <S.MenuWrapper>
+          <S.MenuBar>
+            <NavLink
+              to="/project"
+              style={({ isActive }) => ({ color: isActive ? 'cyan' : 'black' })}
+            >
+              PROJECTS
+            </NavLink>
+            <NavLink
+              to="/information"
+              style={({ isActive }) => ({ color: isActive ? 'cyan' : 'black' })}
+            >
+              STUDIO INFO.
+            </NavLink>
+            <a
+              href="https://usubgi.cafe24.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              BIKABUKA
+            </a>
+          </S.MenuBar>
+          <S.MenuSideBar>
+            <S.ControllBtnWrapper>
+              <div>+</div>
+              <div>/</div>
+              <div>-</div>
+            </S.ControllBtnWrapper>
+            <S.SearchWrapper>
+              <input placeholder="Search" />
+            </S.SearchWrapper>
+          </S.MenuSideBar>
+        </S.MenuWrapper>
+      </S.HeaderContainer>
     </S.Header>
   );
 };
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -5,15 +5,48 @@ export const Header = styled.div`
   line-height: 18px;
   width: 100%;
   min-width: 1000px;
-  background: #444;
-  //margin: -24px;
   padding: 24px;
-  height: 20px;
+  min-height: 30px;
   z-index: 1000;
+  text-align: start;
+`;
+export const HeaderContainer = styled.div`
+  padding: 0 36px;
+  position: relative;
+  display: flex;
+  flex-flow: column;
 `;
 
-export const MenuBar = styled.div`
+export const LogoLink = styled.h1`
+  padding: 15px 0;
+`;
+
+export const MenuWrapper = styled.div`
+  display: flex;
+`;
+
+export const MenuBar = styled.ul`
   width: 700px;
-  margin-left: 36px;
-  float: left;
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  gap: 25px;
+  flex-grow: 3;
+`;
+
+export const MenuSideBar = styled.div`
+  display: flex;
+  flex-grow: 1;
+`;
+
+export const ControllBtnWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 60px;
+`;
+
+export const SearchWrapper = styled.div`
+  padding-left: 30px;
 `;
